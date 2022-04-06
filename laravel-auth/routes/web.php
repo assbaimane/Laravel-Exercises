b9dd0d35a8dd4d17f0120919fa7b73a776318a87
Imane <assbaimane@hotmail.com>
2022-04-06 10:09:29 +0200

    New Auth project created

--- laravel-auth/routes/web.php
+++ laravel-auth/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    // dd(Auth::user()->name); //On peut récupérer comme ça le nom de la personne qui vient de se connecter
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
